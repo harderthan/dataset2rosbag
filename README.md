6ba93d0f7eff57b92e42706f1a849a5716a5481f
harderthan <kheo1772@gmail.com>
2018-09-27 17:21:24 +0900

    *First Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# DataSet to ROSBag
+
+---
+
+##### LICENSE
+
+MIT
+
+---
+
+by harderthan, kheo1772@gmail.com
---
+++ src/dataset2rosbag_node.cpp
@@ -0,0 +1,9 @@
+// ROS
+#include <ros/ros.h>
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "dataset2rosbag_node");
+
+    return 0;
+}
